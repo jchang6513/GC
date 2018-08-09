9bb634bf6376772db8629fc30f176369843d68d8
jchang6513@gmail.com <jchang6513@gmail.com>
2018-08-09 12:50:54 +0800

    create the app and views

--- zenOFpython/zenOFpython/index/views.py
+++ zenOFpython/zenOFpython/index/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import RequestContext
+from django.http import HttpResponse
+from .zens import randzen
+import os
+
+def index(request):
+	return render(request, 'index.html', {'zen':randzen()})
--- zenOFpython/zenOFpython/zenOFpython/settings.py
+++ zenOFpython/zenOFpython/zenOFpython/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'index',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'zenOFpython.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates').replace('\\', '/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
